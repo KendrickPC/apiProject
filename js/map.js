5724a263176fa11959ba722d3e9c03fc6f54cae9
KendrickPC <kennethpchang@gmail.com>
2017-03-23 15:10:05 +0800

    feature: show and hide buttons added

--- js/map.js
+++ js/map.js
@@ -1,8 +1,100 @@
+
+// Begin Initializing Google Maps Javascript API
+// var map;
+// function initMap() {
+// 	map = new google.maps.Map(document.getElementById('map'), {
+//   	center: {lat: 25.0420, lng: 121.5327},
+//   	zoom: 13
+//   });
+// }
+// End Initializing Google Maps Javascript API
+
+
 var map;
+
+// Create a new blank array for all the listing markers.
+var markers = [];
+
+// Menu Toggle Script
+$("#menu-toggle").click(function(e) {
+    e.preventDefault();
+    $("#wrapper").toggleClass("toggled");
+});
+
 function initMap() {
-	map = new google.maps.Map(document.getElementById('map'), {
-  	center: {lat: 25.0420, lng: 121.5327},
-  	zoom: 13
-  });
+    // Constructor creates a new map - only center and zoom are required.
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 25.0422, lng: 121.5083},
+        zoom: 12,
+        mapTypeControl: true
+    });
+
+    // These are the real estate listings that will be shown to the user.
+    // Normally we'd have these in a database instead.
+    var locations = [
+        {title: 'Taipei Main MRT Station', location: {lat: 25.0464, lng: 121.5176}},
+        {title: 'Nanjing Fuxing MRT Station', location: {lat: 25.0522, lng: 121.5440}},
+        {title: 'Zhongxiao MRT Fuxing', location: {lat: 25.0410, lng: 121.5438}},
+        {title: 'Dazhi MRT Station', location: {lat: 25.0795, lng: 121.5469}},
+        {title: 'Ximen MRT Station', location: {lat: 25.0422, lng: 121.5083}},
+        {title: 'Taipei 101', location: {lat: 25.0340, lng: 121.5645}}
+    ];
+
+    var largeInfowindow = new google.maps.InfoWindow();
+
+    // The following group uses the location array to create an array of markers on initialize.
+    for (var i = 0; i < locations.length; i++) {
+        // Get the position from the location array.
+        var position = locations[i].location;
+        var title = locations[i].title;
+        // Create a marker per location, and put into markers array.
+        var marker = new google.maps.Marker({
+            position: position,
+            title: title,
+            animation: google.maps.Animation.DROP,
+            id: i
+        });
+        // Push the marker to our array of markers.
+        markers.push(marker);
+        // Create an onclick event to open an infowindow at each marker.
+        marker.addListener('click', function() {
+            populateInfoWindow(this, largeInfowindow);
+        });
+    }
+    document.getElementById('show-listings').addEventListener('click', showListings);
+    document.getElementById('hide-listings').addEventListener('click', hideListings);
 }
 
+// This function populates the infowindow when the marker is clicked. We'll only allow
+// one infowindow which will open at the marker that is clicked, and populate based
+// on that markers position.
+function populateInfoWindow(marker, infowindow) {
+    // Check to make sure the infowindow is not already opened on this marker.
+    if (infowindow.marker != marker) {
+        infowindow.marker = marker;
+        infowindow.setContent('<div>' + marker.title + '</div>');
+        infowindow.open(map, marker);
+        // Make sure the marker property is cleared if the infowindow is closed.
+        infowindow.addListener('closeclick', function() {
+            infowindow.marker = null;
+        });
+    }
+}
+
+// This function will loop through the markers array and display them all.
+function showListings() {
+    var bounds = new google.maps.LatLngBounds();
+    // Extend the boundaries of the map for each marker and display the marker
+    for (var i = 0; i < markers.length; i++) {
+        markers[i].setMap(map);
+        bounds.extend(markers[i].position);
+    }
+    map.fitBounds(bounds);
+}
+
+// This function will loop through the listings and hide them all.
+function hideListings() {
+    for (var i = 0; i < markers.length; i++) {
+        markers[i].setMap(null);
+    }
+}

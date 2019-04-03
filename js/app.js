90f911807d792d12aa7d30572c85e4bc14632bac
kendrickpc <kennethpchang@gmail.com>
2019-04-03 17:56:56 +0800

    Working template with map loaded and API calls work correctly.

--- js/app.js
+++ js/app.js
@@ -1,14 +1,69 @@
-var Restaurant = function() {
+// Foursquare API info:
+// client ID: WD2PWD3XXQGUBSXDTKXVFE2Y3CNB03FRPTXSORFUHGUVVGPO
+// client secret: YHLXV3YGUTIBM45TVR3AK1JZTTUURFZUKXMNKEOMXSFOQLXD
+// v: <YEAR/MO/DY>
+
+var foursquareUrl = 'https://api.foursquare.com/v2/venues/';
+var foursquareParams = $.param({
+    'client_id': 'WD2PWD3XXQGUBSXDTKXVFE2Y3CNB03FRPTXSORFUHGUVVGPO',
+    'client_secret': 'YHLXV3YGUTIBM45TVR3AK1JZTTUURFZUKXMNKEOMXSFOQLXD',
+    'v': '20190402'
+});
+
+var restObjArray = [
+    {
+        'name': 'Cuckoos Nest',
+        'id': '4cd6bc2baeb1224b80df29ff',
+        'notes': 'Cuckoos Nest'
+    }
+];
+
+var Restaurant = function(restObj) {
     self = this;
-    self.marker = {};
-    self.coordinates = {lat: 25.083390, lng: 121.551250};
+    self.id = restObj.id;
+    self.name = restObj.name;
+    self.description = restObj.notes;
+
+    self.api_url = foursquareUrl + self.id + '?' + foursquareParams;
+
+    $.ajax({
+        url: self.api_url,
+        data: {format: 'json'},
+        dataType: 'json'
+    }).done(function(data){
+        console.log(data);
+        console.log('Successfully queried Foursquare API for ' + self.name);
+    }).fail(function(){
+        console.log('Foursquare API failed for ' + self.name);
+    });
+
+    self.coordinates = {lat: 37.780679, lng: -122.396086};
+    self.img_url = 'https://fastly.4sqi.net/img/general/600x600/5523591_GPT3g8CEdK1oMe8bvXcGfBHAgSV65-zvuDAgsmtz23Q.jpg';
+    self.url = 'https://foursquare.com/v/%E6%9D%9C%E9%B5%91%E7%AA%A9%E5%92%96%E5%95%A1-cockoos-nest/4cd6bc2baeb1224b80df29ff';
+    self.rest_url = 'http://espn.com';
+
+    var img_size = '100x100';
+
+    var contentString = $( '#info-template' ).html();
+
+    contentString = contentString.replace( '{{title}}', self.name )
+                                 .replace( '{{img_url}}', self.img_url )
+                                 .replace( '{{rest_url}}', self.rest_url)
+                                 .replace( '{{url}}', self.url )
+                                 .replace( '{{content}}', self.description );
+
+    self.infoWindow = new google.maps.InfoWindow({
+        content: contentString
+    });
+
 };
 
 var ViewModel = function() {
     var self = this;
+
     self.kickoff = function() {
-        this.rest = new Restaurant();
-        addMarker(this.rest, map);
+        self.rest = new Restaurant(restObjArray[0]);
+        addMarker(self.rest, map);
     };
 };
 
--- js/script.js
+++ js/script.js
@@ -12,35 +12,20 @@ function initMap() {
 }
 
 function addMarker(restObj, map){
-    var contentString = '<div id="content">'+
-        '<div id="siteNotice">'+
-        '</div>'+
-        '<h1 id="firstHeading" class="firstHeading">Taipei</h1>'+
-        '<div id="bodyContent">'+
-        '<p><b>Taipei</b> is my<br><br>' +
-        'home.</p>'
-        '</div>'+
-        '</div>';
-
     marker = new google.maps.Marker({
         position: restObj.coordinates,
         animation: google.maps.Animation.DROP,
         map: map,
-        // label: 'What up?',
         title: 'Hello Kenneth!'
-    });
-
-    var infoWindow = new google.maps.InfoWindow({
-        content: contentString
     });
 
     // marker.addListener('click', toggleBounce);
     marker.addListener('click', function(){
         marker.setAnimation(4);
-        infoWindow.open(map, marker);
+        restObj.infoWindow.open(map, marker);
     });
 
     map.addListener('click', function(){
-        infoWindow.close();
+        restObj.infoWindow.close();
     });
 }

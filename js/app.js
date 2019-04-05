8f1d8899ab148f3e10b8d215ae0d5af1cdae40f5
kendrickpc <kennethpchang@gmail.com>
2019-04-05 16:15:09 +0800

    Refactoring app.js from script.js

--- js/app.js
+++ js/app.js
@@ -3,7 +3,20 @@
 // client secret: YHLXV3YGUTIBM45TVR3AK1JZTTUURFZUKXMNKEOMXSFOQLXD
 // v: <YEAR/MO/DY>
 
-self.coordinates = {lat: 25.083390, lng: 121.551250};
+// 650 號明水路 lat: 25.083389, lng: 121.551247
+// Cuckoo's Nest: lat: 25.083390, lng: 121.551250
 
-var viewM = new ViewModel();
-ko.applyBindings( viewM );
+
+// Places Library for Javascript
+// https://developers.google.com/maps/documentation/javascript/places
+// var map = {};
+// var marker = {};
+// var infoWindow = {};
+
+
+// Activates Knockout JS
+// http://learn.knockoutjs.com/#/?tutorial=intro
+// var viewM = new ViewModel();
+// ko.applyBindings( viewM );
+
+
